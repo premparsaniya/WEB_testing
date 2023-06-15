2a8753830e89f96ef354eeda2b090b0eb9f59a14
premparsaniya <premparsaniya.ubs@gmail.com>
2023-06-15 13:21:08 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
+// import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Toaster />
+      {/* <Toaster /> */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
